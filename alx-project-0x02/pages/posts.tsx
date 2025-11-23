8adc184672aceda073f0e48d2a6ddf3f24acf9b6
Billyrono <billyrono46@gmail.com>
2025-11-23 23:45:19 +0300

    Implement PostCard component and fetch posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,7 +1,7 @@
 import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 
 const Posts: NextPage = () => {
@@ -11,12 +11,10 @@ const Posts: NextPage = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(
-          "https://jsonplaceholder.typicode.com/posts?_limit=10"
-        );
+        const res = await fetch("https://dummyjson.com/posts?limit=12");
         const data = await res.json();
 
-        const formattedPosts: PostProps[] = data.map((post: any) => ({
+        const formattedPosts = data.posts.map((post: any) => ({
           title: post.title,
           content: post.body,
           userId: post.userId,
@@ -35,23 +33,35 @@ const Posts: NextPage = () => {
   return (
     <div>
       <Header />
-      <h1 className="text-2xl font-bold text-center mt-10">Posts</h1>
-      {loading ? (
-        <p className="text-center mt-4">Loading posts...</p>
-      ) : (
-        <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
-          {posts.map((post, idx) => (
-            <PostCard
-              key={idx}
-              title={post.title}
-              content={post.content}
-              userId={post.userId}
-            />
-          ))}
-        </div>
-      )}
+      <h1 className="text-2xl font-bold text-center mt-10">Posts Page</h1>
+
+      <main className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {posts.map((post, idx) => (
+          <PostCard
+            key={idx}
+            title={post.title}
+            content={post.content}
+            userId={post.userId}
+          />
+        ))}
+      </main>
     </div>
   );
 };
 
 export default Posts;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://dummyjson.com/posts?limit=12");
+  const data = await res.json();
+
+  const posts: PostProps[] = data.posts.map((p: any) => ({
+    title: p.title,
+    content: p.body,
+    userId: p.userId,
+  }));
+
+  return {
+    props: { posts },
+  };
+};
