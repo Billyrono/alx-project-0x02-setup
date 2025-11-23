0b45675057be5bb1328ea7da81ee692a6dbaf6b3
Billyrono <billyrono46@gmail.com>
2025-11-23 21:19:20 +0300

    Setup Next.js project, add Header and Home page

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,18 +1,16 @@
-// components/layout/Header.tsx
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
       <h1 className="text-xl font-semibold">ALX Project Header</h1>
-      <li>
-        <a href="/home" className="mr-4 hover:underline">
-          Home
-        </a>
-        <a href="/about" className="mr-4hover:underline">
-          About
-        </a>
-      </li>
+      <Link href="/home" className="mr-4 hover:underline">
+        Home
+      </Link>
+      <Link href="/about" className="mr-4 hover:underline">
+        About
+      </Link>
     </header>
   );
 };
